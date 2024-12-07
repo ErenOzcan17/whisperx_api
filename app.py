71abe1bbc73c347f244688bf7e8ae4c04423b429
Eren Özcan <qwerrty38@outlook.com>
2024-12-07 03:05:32 +0300

    app.py dosyası düzenlendi

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+import json
+from flask import Flask, request, jsonify, Response
 import whisperx
 import torch
 import os
@@ -6,41 +7,29 @@ import os
 # Flask uygulaması başlatılıyor
 app = Flask(__name__)
 
-# WhisperX modelini yükleyin
-device = "cuda" if torch.cuda.is_available() else "cpu"  # GPU varsa CUDA kullanılır, yoksa CPU'ya geçilir
-model = whisperx.load_model("medium", device=device)
-
-# Bellek temizleme
-torch.cuda.empty_cache()
+if torch.cuda.is_available():
+    model = whisperx.load_model("medium", device="cuda")
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe_audio():
-    # Ses dosyasını alın
-    if 'audio' not in request.files:
-        return jsonify({'error': 'No audio file provided'}), 400
-
-    audio_file = request.files['audio']
-
-    # Geçici bir dosyaya kaydedin
-    audio_path = "temp_audio.wav"
-    audio_file.save(audio_path)
-
-    # Ses dosyasını transkribe edin (Batch boyutunu burada kontrol edebilirsiniz)
     try:
-        # Eğer batch boyutunu kontrol etmek istiyorsanız, 'batch_size' parametresini modelin transcribe fonksiyonuna verebilirsiniz.
-        result = model.transcribe(audio_path, batch_size=1)  # Batch boyutunu 1 yaparak belleği optimize ediyoruz
+        audio_file = request.files['audio']
+        # Sesi geçici bir dosyaya kaydediyorum
+        audio_path = "temp_audio.wav"
+        audio_file.save(audio_path)
+    except KeyError:
+        return Response(response="Audio file is missing in the request", status=400)
+    except Exception:
+        return Response(response="An error occurred while take the sound file", status=500)
+
+    try:
+        result = model.transcribe(audio_path, batch_size=16)
     except RuntimeError as e:
-        if "CUDA out of memory" in str(e):
-            torch.cuda.empty_cache()  # Belleği temizle
-            return jsonify({'error': 'CUDA out of memory, try reducing batch size or using CPU'}), 500
-        else:
-            raise e
+        return Response(response="An error occurred while processing the audio file", status=500)
 
-    # Geçici dosyayı silin
+    # Geçici dosyayı siliyorum
     os.remove(audio_path)
-
-    # Sonuçları JSON olarak döndürün
-    return jsonify(result)
+    return Response(json.dumps(result, ensure_ascii=False))
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
